9acadc0e491e6bd27b1f98adae971e02e55e67c5
Lyaiya <hipsnafoha@outlook.com>
2022-03-25 00:30:22 +0800

    Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.atrin.humidweather"
         minSdk = 24
         targetSdk = 32
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
