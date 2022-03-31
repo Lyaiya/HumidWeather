3643958388011f63d5da55e18fea1537cc124a98
Lyaiya <hipsnafoha@outlook.com>
2022-03-31 15:06:57 +0800

    Bump version to 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.atrin.humidweather"
         minSdk = 24
         targetSdk = 32
-        versionCode = 4
-        versionName = "1.2.2"
+        versionCode = 5
+        versionName = "1.2.3"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
