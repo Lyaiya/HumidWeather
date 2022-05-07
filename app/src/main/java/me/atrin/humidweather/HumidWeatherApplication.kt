0d25029318751086599b41632dc5cdfb4bfe31f2
Lyaiya <hipsnafoha@outlook.com>
2022-05-07 16:43:20 +0800

    Init Logger

--- app/src/main/java/me/atrin/humidweather/HumidWeatherApplication.kt
+++ app/src/main/java/me/atrin/humidweather/HumidWeatherApplication.kt
@@ -1,12 +1,17 @@
 package me.atrin.humidweather
 
 import android.app.Application
+import com.dylanc.longan.initLogger
 
 class HumidWeatherApplication : Application() {
 
     companion object {
         // OPTIMIZE: 密钥应存储在本地
         const val TOKEN = "GQG7IE2PXti6FpBV"
+    }
+
+    init {
+        initLogger(BuildConfig.DEBUG)
     }
 
 }
