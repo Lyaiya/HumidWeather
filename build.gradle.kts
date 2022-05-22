8e6b6de16724c002345fcc55461f831273bbaeee
Lyaiya <hipsnafoha@outlook.com>
2022-05-22 13:54:04 +0800

    Update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.devtools.ksp) apply false
 }
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
